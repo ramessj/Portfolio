8ed2b9574c734e77499b86ef9497fe3281b026bb
Ramiro Quesada <ramiroquesadaperez@gmail.com>
2023-06-09 12:45:18 -0300

    julien nasi

--- src/App.jsx
+++ src/App.jsx
@@ -13,16 +13,10 @@ export const App = () => {
 			{!isMobile && (
 				<AnimatedCursor
 					innerSize={20}
-					outerSize={900}
-					color="925, 211, 111"
-					innerScale={0.7}
-					trailingSpeed={18}
-					showSystemCursor={false}
-					outerStyle={{
-						pointerEvents: 'none',
-						inset: '-1px',
-						background: `radial-gradient(800px circle,rgba(255,255,255,.1), transparent 40%)`,
-					}}
+					outerSize={15}
+					color="255, 255, 165"
+					innerScale={0}
+					trailingSpeed={1}
 					clickables={[
 						'a',
 						'input[type="text"]',
--- src/portfolio/routes/HomePage/Home1.jsx
+++ src/portfolio/routes/HomePage/Home1.jsx
@@ -1,6 +1,8 @@
 // import { useSelector } from "react-redux";
 // import { getTexts } from "../../../helpers/getTexts";
 
+import { motion } from 'framer-motion';
+
 export const Home1 = () => {
 	// const {lang} = useSelector( state => state.ui )
 
@@ -18,9 +20,22 @@ export const Home1 = () => {
 				</div>
 
 				<div>
-					<h2>
-						Hello <span className="hellodot">.</span>
+					<h2 className="helloh2">
+						Hello
+						<motion.span
+							className="hellodot"
+							style={{ background: '#FFF', borderRadius: '50%', marginLeft: '1rem', marginRight:'1rem', alignSelf: 'end' }}
+							animate={{
+								y: ['20%', '-185%', '-200%', '-185%', '20%'],
+								width: ['2.5rem', '2rem', '1.5rem', '2rem', '2.5rem'],
+								height: ['1.5rem', '1.9rem', '2rem',  '1.9rem', '1.5rem'],
+							}}
+							transition={{
+								repeat: Infinity,
+								duration: 1,
+							}}></motion.span>
 					</h2>
+
 					<h2>I am</h2>
 					<h2>Ramiro</h2>
 				</div>
