27062b3f37a3ac20fa33058c1413f05379dcb777
Ramiro Quesada <ramiroquesadaperez@gmail.com>
2023-06-09 05:53:25 -0300

    home1 desing start

--- src/portfolio/routes/HomePage/Home1.jsx
+++ src/portfolio/routes/HomePage/Home1.jsx
@@ -2,16 +2,29 @@
 // import { getTexts } from "../../../helpers/getTexts";
 
 export const Home1 = () => {
-
 	// const {lang} = useSelector( state => state.ui )
 
 	// const texts = getTexts('home1', lang);
 
-
 	return (
 		<>
-			<h2>HOME1</h2>
-			{/* <p>{texts.home1}</p> */}
+			<div className="home1container">
+				<div>
+					<div></div>
+					<div>
+						<h3>Im a Programmer</h3>
+					</div>
+					<div></div>
+				</div>
+
+				<div>
+					<h2>
+						Hello <span className="hellodot">.</span>
+					</h2>
+					<h2>I am</h2>
+					<h2>Ramiro</h2>
+				</div>
+			</div>
 		</>
 	);
 };
