2dc260b11513930a7b3e1a2daf90e847e7ee5b95
Ramiro Quesada <ramiroquesadaperez@gmail.com>
2023-06-11 22:49:35 -0300

    advcs

--- src/portfolio/routes/HomePage/components/BouncingBall.jsx
+++ src/portfolio/routes/HomePage/components/BouncingBall.jsx
@@ -10,11 +10,9 @@ export const BouncingBall = () => {
 				marginLeft: '0.5rem',
 				marginRight: '1rem',
 				alignSelf: 'end',
-				// height: '0.5rem'
 			}}
 			animate={{
 				y: ['-50%', '-140%', '-150%', '-140%', '-50%'],
-				// y: ['-0.8rem', '-1.36rem', '-1.5rem', '-1.36rem',  '-0.8rem'],
 				scaleY: [1, 1.1, 1.1, 1.1, 1],
 				scaleX: [1.3, 1.05, 1, 1.05, 1.3],
 			}}
