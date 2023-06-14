ba5fb2d3512f79769c948abc78c9fa2185af0b6b
Ramiro Quesada <ramiroquesadaperez@gmail.com>
2023-06-14 01:46:43 -0300

    techs url fixed

--- src/assets/webSkills/index.js
+++ src/assets/webSkills/index.js
@@ -1,52 +1,69 @@
+import html5 from './html5.png';
+import html5BW from './html5BW.png'
+import css from './css3.png'
+import cssBW from './css3BW.png'
+import javascript from './javascript.png'
+import javascriptBW from './javascriptBW.png'
+import reactC from './react.png'
+import reactBW from './reactBW.png'
+import reduxC from './redux.png'
+import reduxBW from './reduxBW.png'
+import npm from './npm.png'
+import npmBW from './npmBW.png'
+import nodejs from './node-js.png'
+import nodejsBW from './node-jsBW.png'
+import mongodb from './mongodb.png'
+import mongodbBW from './mongodbBW.png'
+import git from './git.png'
+import gitBW from './gitBW.png'
 
-const path = 'src/assets/webSkills/'
 
 const techs = [	{
 	name: 'HTML',
-	color: path + 'html5.png',
-	bw: path + 'html5BW.png',
+	color: html5,
+	bw: html5BW,
 },
 	{
 		name: 'CSS',
-		color: path + 'css3.png',
-		bw: path + 'css3BW.png',
+		color: css,
+		bw: cssBW,
 	},
 	
 	{
 		name: 'JavaScript',
-		color: path + 'javascript.png',
-		bw: path + 'javascriptBW.png',
+		color: javascript,
+		bw: javascriptBW,
 	},
 	{
 		name: 'ReactJS',
-		color: path + 'react.png',
-		bw: path + 'reactBW.png',
+		color: reactC,
+		bw: reactBW,
 	},
 	{
 		name: 'Redux',
-		color: path + 'redux.png',
-		bw: path + 'reduxBW.png',
+		color: reduxC,
+		bw: reduxBW,
 	},
 	{
 		name: 'NPM',
-		color: path + 'npm.png',
-		bw: path + 'npmBW.png',
+		color:npm,
+		bw: npmBW,
 	},
 	
 	{
 		name: 'NodeJS',
-		color: path + 'node-js.png',
-		bw: path + 'node-jsBW.png',
+		color: nodejs,
+		bw: nodejsBW,
 	},
 	{
 		name: 'MongoDB',
-		color: path + 'mongodb.png',
-		bw: path + 'mongodbBW.png',
+		color: mongodb,
+		bw: mongodbBW,
 	},
 	{
 		name: 'GIT',
-		color: path + 'git.png',
-		bw: path + 'gitBW.png',
+		color:git,
+		bw: gitBW,
 	},
 
 ];
