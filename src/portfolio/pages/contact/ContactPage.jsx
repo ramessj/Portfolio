d72b6545a452ebd3973d2b686b1e8acd50801287
Ramiro Quesada <ramiroquesadaperez@gmail.com>
2023-06-27 20:53:18 -0300

    advcs

--- src/portfolio/pages/contact/ContactPage.jsx
+++ src/portfolio/pages/contact/ContactPage.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Lines } from '../../components';
-import { motion } from 'framer-motion';
+// import { motion } from 'framer-motion';
 
 export const ContactPage = () => {
 	const myRef = useRef();
@@ -21,46 +21,15 @@ export const ContactPage = () => {
 
 	return (
 		<>
-			<div className='home2container' ref={myRef}>
-			<div className='home2right'>
-					<motion.div
-						className='imgDragContainer'
-						initial={{ y: 0, dragListener: false }}
-						drag={true}
-						whileDrag={{ scale: 1.2 }}
-						dragSnapToOrigin
-						animate={{
-							y: [0, 5, 10, -10, -5, 0],
-							dragListener: true,
-							transition: { duration: 6, repeat: Infinity },
-						}}
-						style={{ touchAction: 'none' }}>
-						<motion.img className='profileImg' alt='Programmer' />
-					</motion.div>
-				</div>
 			
-				<div className='home2left'>
-					<div className='aboutHead'>
-						<div className='aboutHeadText'>
-							<h2>Contact Me</h2>
-						</div>
-						<Lines />
+				<div className='contactContainer'>
+				<h2 className='text-primary'  ref={myRef}>Contact Me!</h2>
+				<Lines/>
+
+					<div className="contactBody">
+						<p>My inbox is always open. Whether you have a question or project, or just want to say hi, i&apos;ll try my best to get back to you!</p>
+						<button className='btn btn-primary'>Get in touch</button>
 					</div>
-					<div className='aboutBody'>
-						<p>
-							I&apos;m Ramiro Quesada, a passionate Front-End
-							Developer based in Uruguay with 1 year of
-							experience. Currently expanding my knowledge to
-							become a Full-Stack Developer through project-based
-							learning. I enjoy exploring all aspects of Computer
-							Science and constantly learning new technologies.
-							<br />
-							<br /> Let&apos;s connect and bring your ideas to
-							life!
-						</p>
-						
-					</div>
-				</div>
 				</div>
 		</>
 	);
