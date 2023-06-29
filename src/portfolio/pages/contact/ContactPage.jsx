37b5bd12b1f5bf170dd8b1df564313ff5ed8139e
Ramiro Quesada <ramiroquesadaperez@gmail.com>
2023-06-29 17:48:54 -0300

    sections height and other visual fixes, more yellow less red

--- src/portfolio/pages/contact/ContactPage.jsx
+++ src/portfolio/pages/contact/ContactPage.jsx
@@ -2,7 +2,8 @@
 import { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Lines } from '../../components';
-// import { motion } from 'framer-motion';
+
+import './contactpage.css';
 
 export const ContactPage = () => {
 	const myRef = useRef();
@@ -21,16 +22,23 @@ export const ContactPage = () => {
 
 	return (
 		<>
-			
-				<div className='contactContainer'>
-				<h2 className='text-primary'  ref={myRef}>Contact Me!</h2>
-				<Lines/>
-
-					<div className="contactBody">
-						<p>My inbox is always open. Whether you have a question or project, or just want to say hi, i&apos;ll try my best to get back to you!</p>
-						<button className='btn btn-primary'>Get in touch</button>
-					</div>
+			<div className='contactContainer'>
+				<div className='contactTitle'>
+					<h2 className='text-primary' ref={myRef}>
+						Contact Me!
+					</h2>
+					<Lines />
 				</div>
+
+				<div className='contactBody'>
+					<p>
+						My inbox is always open. Whether you have a question or
+						project, or just want to say hi, i&apos;ll try my best
+						to get back to you!
+					</p>
+					<button className='btn btn-primary'>Get in touch</button>
+				</div>
+			</div>
 		</>
 	);
 };
