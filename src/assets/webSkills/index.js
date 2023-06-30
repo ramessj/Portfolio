346ff95d2207e9738cd7abc86f11e52df5b42d37
Ramiro Quesada <ramiroquesadaperez@gmail.com>
2023-06-29 23:51:39 -0300

    advcs

--- src/assets/webSkills/index.js
+++ src/assets/webSkills/index.js
@@ -11,57 +11,113 @@ import express from './express.png';
 import firebase from './firebase.png';
 import framer from './framer.png';
 
-const techs = [
-	{
-		name: 'HTML',
-		color: html5,
-	},
-	{
-		name: 'CSS',
-		color: css,
-	},
+const techs = {
+	"front": [
+		{
+			name: 'HTML',
+			color: html5,
+		},
+		{
+			name: 'CSS',
+			color: css,
+		},
+		{
+			name: 'JavaScript',
+			color: javascript,
+		},
+		{
+			name: 'ReactJS',
+			color: reactC,
+		},
+		{
+			name: 'Redux',
+			color: reduxC,
+		},
+		{
+			name: 'Framer Motion',
+			color: framer,
+		}
+	],
+	"back":[
+		{
+			name: 'Express',
+			color: express,
+		},
+		{
+			name: 'MongoDB',
+			color: mongodb,
+		},
+		{
+			name: 'NodeJS',
+			color: nodejs,
+		},
+		{
+			name: 'Firebase',
+			color: firebase,
+		}
+	],
+	"tools": [
+		{
+			name: 'GIT',
+			color: git,
+		},
+		{
+			name: 'NPM',
+			color: npm,
+		}
+	],
+};
+// const techs = [
+// 	{
+// 		name: 'HTML',
+// 		color: html5,
+// 	},
+// 	{
+// 		name: 'CSS',
+// 		color: css,
+// 	},
 
-	{
-		name: 'JavaScript',
-		color: javascript,
-	},
-	{
-		name: 'ReactJS',
-		color: reactC,
-	},
-	{
-		name: 'Redux',
-		color: reduxC,
-	},
+// 	{
+// 		name: 'JavaScript',
+// 		color: javascript,
+// 	},
+// 	{
+// 		name: 'ReactJS',
+// 		color: reactC,
+// 	},
+// 	{
+// 		name: 'Redux',
+// 		color: reduxC,
+// 	},
 
-	{
-		name: 'NodeJS',
-		color: nodejs,
-	},
-	{
-		name: 'Express',
-		color: express,
-	},
-	{
-		name: 'MongoDB',
-		color: mongodb,
-	},
-	{
-		name: 'GIT',
-		color: git,
-	},
-	{
-		name: 'NPM',
-		color: npm,
-	},
-	{
-		name: 'Firebase',
-		color: firebase,
-	},
-	{
-		name: 'Framer Motion',
-		color: framer,
-	},
-];
+// 	{
+// 		name: 'NodeJS',
+// 		color: nodejs,
+// 	},
+// 	{
+// 		name: 'Express',
+// 		color: express,
+// 	},
+// 	{
+// 		name: 'MongoDB',
+// 		color: mongodb,
+// 	},
+// 	{
+// 		name: 'GIT',
+// 		color: git,
+// 	},
+// 	{
+// 		name: 'NPM',
+// 		color: npm,
+// 	},
+// 	{
+// 		name: 'Firebase',
+// 		color: firebase,
+// 	},
+// 	{
+// 		name: 'Framer Motion',
+// 		color: framer,
+// 	},
+// ];
 
 export default techs;
--- src/portfolio/components/WebSkills/WebSkills.jsx
+++ src/portfolio/components/WebSkills/WebSkills.jsx
@@ -1,27 +1,71 @@
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+
 import techs from '../../../assets/webSkills';
 import './webskills.css';
 
 export const WebSkills = () => {
+	const [techType, setTechType] = useState('front');
+
+	let techsData = techs[techType];
+
+	const setTech = (e) => {
+		const data = e.target.getAttribute('data');
+		if (techType != data) {
+			setTechType(data);
+			techsData = techs[techType];
+		}
+	};
+
 	return (
 		<>
 			<div className='webSkillsContainer'>
-				<h3>Front-End</h3>
+				<div className='techTypeButtons'>
+					<button
+						className={`techTypeButton  ${
+							techType == 'front' ? 'activeTech' : ''
+						}`}
+						onClick={setTech}
+						data='front'>
+						Front-End
+					</button>
+					<button
+						className={`techTypeButton  ${
+							techType == 'back' ? 'activeTech' : ''
+						}`}
+						onClick={setTech}
+						data='back'>
+						Back-End
+					</button>
+					<button
+						className={`techTypeButton  ${
+							techType == 'tools' ? 'activeTech' : ''
+						}`}
+						onClick={setTech}
+						data='tools'>
+						Tools
+					</button>
+				</div>
+
 				<div className='technologies'>
-					{techs.map((tech) => (
-						<div className='techContainer' key={tech.name}>
+					{techsData.map((tech) => (
+						<motion.div
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							className='techContainer'
+							key={tech.name}>
 							{/* <div className='tech'> */}
-								<img
-									src={tech.color}
-									alt={tech.name}
-									className='tech techLogo'
-								/>
+							<img
+								src={tech.color}
+								alt={tech.name}
+								className='tech techLogo'
+							/>
 							{/* </div> */}
 							<div className='techName'>{tech.name}</div>
-						</div>
+						</motion.div>
 					))}
 				</div>
 			</div>
-			
 		</>
 	);
 };
