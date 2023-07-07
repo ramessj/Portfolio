112ce4f1be2712c6d61917c58fc573818f50fb16
Ramiro Quesada <ramiroquesadaperez@gmail.com>
2023-07-07 05:28:31 -0300

    fullpage implementada

--- src/portfolio/components/Parallax/Parallax.jsx
+++ src/portfolio/components/Parallax/Parallax.jsx
@@ -2,21 +2,41 @@
 // import { motion } from 'framer-motion';
 
 import './parallax.css';
-
+import ReactFullpage from '@fullpage/react-fullpage';
 export const Parallax = ({ sections }) => {
-
 	return (
 		<>
-			{sections.map((section) => (
+			{/* {sections.map((section) => (
 				<div
 					className='sectionWrapper container'
 					key={sections.indexOf(section) + 1}
 					id={section.props.id}>
-					<section >
-						{section}
-					</section>
+					<section>{section}</section>
 				</div>
-			))}
+			))} */}
+
+			<ReactFullpage
+				//fullpage options
+				licenseKey={'YOUR_KEY_HERE'}
+				scrollingSpeed={750} /* Options here */
+				render={() => {
+					return (
+						<ReactFullpage.Wrapper>
+							{sections.map((section) => (
+								// <div
+								// 	className='section sectionWrapper container'
+								// 	key={sections.indexOf(section) + 1}
+								// 	id={section.props.id}>
+
+								// </div>
+								<section className='section container' key={sections.indexOf(section) + 1} id={section.props.id}>
+									{section}
+								</section>
+							))}
+						</ReactFullpage.Wrapper>
+					);
+				}}
+			/>
 		</>
 	);
 };
