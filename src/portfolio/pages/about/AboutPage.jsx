91ca64b4ca1a8216edee69ccacc797c208e56c68
Ramiro Quesada <ramiroquesadaperez@gmail.com>
2023-07-08 05:20:49 -0300

    advcs

--- src/portfolio/pages/about/AboutPage.jsx
+++ src/portfolio/pages/about/AboutPage.jsx
@@ -4,6 +4,14 @@ import { motion } from 'framer-motion';
 import { Tooltip } from 'react-tooltip';
 import './aboutpage.css';
 
+const tooltipStyle = {
+	marginTop: '0.5rem',
+	fontWeight: 600,
+	fontSize: '1rem',
+	background: 'black',
+	
+}
+
 export const AboutPage = () => {
 	const { t } = useTranslation();
 
@@ -56,7 +64,7 @@ export const AboutPage = () => {
 							<div
 								className='hex'
 								data-tooltip-id='comms'
-								data-tooltip-content='Communication Skills'>
+								data-tooltip-content='Good Communication'>
 								<div className='hexTextContainer'>
 									<i className='fa-solid fa-comment-dots'></i>
 								</div>
@@ -64,9 +72,9 @@ export const AboutPage = () => {
 							<div
 								className='hex'
 								data-tooltip-id='problem'
-								data-tooltip-content='Problem solver'>
+								data-tooltip-content='Problem Solver'>
 								<div className='hexTextContainer'>
-									<i className='fa-solid fa-lightbulb'></i>
+									<i className="fas fa-puzzle-piece"></i>
 								</div>
 							</div>
 							<div
@@ -81,43 +89,22 @@ export const AboutPage = () => {
 						<Tooltip
 							id='autodidact'
 							place='bottom'
-							style={{
-								marginTop: '0.5rem',
-								fontWeight: 600,
-								fontSize: '1rem',
-								background: 'black',
-								
-							}}
+							style={tooltipStyle}
 						/>
 						<Tooltip
 							id='comms'
 							place='bottom'
-							style={{
-								marginTop: '0.5rem',
-								fontWeight: 600,
-								fontSize: '1rem',
-								background: 'black',
-							}}
+							style={tooltipStyle}
 						/>
 						<Tooltip
 							id='problem'
 							place='bottom'
-							style={{
-								marginTop: '0.5rem',
-								fontWeight: 600,
-								fontSize: '1rem',
-								background: 'black',
-							}}
+							style={tooltipStyle}
 						/>
 						<Tooltip
 							id='teamwork'
 							place='bottom'
-							style={{
-								marginTop: '0.5rem',
-								fontWeight: 600,
-								fontSize: '1rem',
-								background: 'black',
-							}}
+							style={tooltipStyle}
 						/>
 					</div>
 				</motion.div>
